9f178c934f3e071645f61fc996cd3dd4f62d048c
C-Dewar <133111661+C-Dewar@users.noreply.github.com>
2023-10-18 15:16:23 +0200

    IWA2 Challenges 1,2 & 3

---
+++ IWA2/scripts.js
@@ -0,0 +1,38 @@
+/* CHALLENGE 1 */
+/* This is the firstname of the user */
+//error 1 = constant declaration was commented out through the switching of the '/' ad '*' operators
+//error 2 = console log was declared incorrectly with semicolons being used instead of commas with an extra included
+const user = 'John'
+
+/* This is the lastname of the user */
+ 
+const surname = 'Smith'
+
+
+console.log(user, surname)
+
+/* CHALLENGE 2 */
+
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014'
+
+console.log(date)
+
+/* CHALLENGE 3 */
+
+/* 
+It is important to show the following message in order to:
+* assure users
+* scare hackers
+* impress investors
+*/
+
+console.warn("Security scan starting");
+
+/* It is important to let user know when they can close the page  */
+
+console.info("Please wait for scan to complete before closing the browser");
