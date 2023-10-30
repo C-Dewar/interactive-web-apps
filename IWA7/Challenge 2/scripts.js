634e1e94db066cf5ee144b5594f5ffe1a18b9f7a
C-Dewar <133111661+C-Dewar@users.noreply.github.com>
2023-10-31 01:16:01 +0200

    IWA8 + IWA7 Fix

--- IWA7/Challenge 2/scripts.js
+++ IWA7/Challenge 2/scripts.js
@@ -3,6 +3,7 @@ const firstname = "Timothy";
 
 if(nickname === null && firstname!==null){
 const name = firstname
+console.log("Good Morning, ",name,"!")
 }else{
     const name = nickname
     console.log("Good Morning, ",name,"!")
